352a647869dab8f733fb1378d546491a12bdb246
Fangtastic7 <30578148+Fangtastic7@users.noreply.github.com>
2018-05-11 18:30:08 -0700

    Add files via upload

---
+++ src/Platypus.java
@@ -0,0 +1,17 @@
+public class Platypus {
+      Platypus(String name){
+    	  System.out.println(name);
+      }
+    		  
+      public static void main(String[] args) {
+    	  Platypus p = new Platypus("Perry" );
+    	  System.out.println(p + " the Platypus is smarter than your average platypus.");
+    //  Runner bob = new Runner();
+    //  bob.sayHi("Perry");
+      }
+}
+class Runner {
+      	void sayHi(String p){
+     //       	System.out.println(name + " the Platypus is smarter than your average platypus.");
+      	}
+}
