9a5b7e0b06ed74ae69931d9423bdfe81d8f07dcb
Fangtastic7 <league@iMac-2.attlocal.net>
2018-06-15 18:28:26 -0700

    Vault

--- src/VaultRunner.java
+++ src/VaultRunner.java
@@ -1,66 +1,59 @@
 import java.util.Random;
 
- class Vault {
-	int secretCode;
-	
+class Vault {
+	private int secretCode;
+
 	boolean tryCode(int guess) {
-		if(guess==secretCode) {
-			return   true;
-		}
-		else {
+		if (guess == secretCode) {
+			return true;
+		} else {
 			return false;
 		}
 	}
+
 	public Vault() {
 		Random random = new Random();
 		secretCode = random.nextInt(1000001);
 	}
-		
-		}
-	
-	
+
+}
 
 public class VaultRunner {
 	static Vault object;
 	static JamesBond key;
+
 	public static void main(String[] args) {
 		object = new Vault();
 		key = new JamesBond();
-		key.findCode(object);
-		object.tryCode(key.findCode(object));
-		//if(object.secretCode==key.answer) {
-		System.out.println(object.secretCode +" code has been opened.");
-	//	}
-	//	else {
-			System.out.println("Mission failed. "+ key.answer);
-	//	}
-}
+		int x = key.findCode(object);
+		if(x==-1) {
+			
+		
+		System.out.println(-1);
+		}
+		else {
+			System.out.println("James Bond has broken the Code! The code is: " + x );
+		}
+	
+	}
 }
 
- class JamesBond {
-	
-	 int answer;
-	 int findCode(Vault vault){
-		 Vault path = new Vault();
-	
-		 for(int i =0; i<path.secretCode;i++) {		 
-		
-			 if(i==path.secretCode-1) {
-				 return answer = path.secretCode+1;
-			 }
-			
-			
-		
-		
-}
-		 
-			 return answer;
-		 
-		
-	
-	
-		
-		
+class JamesBond {
+
+	// int answer;
+	int findCode(Vault vault) {
+		Vault path = new Vault();
+
+		for (int i = 0; i < 1000000; i++) {
+			boolean answer = path.tryCode(i);
+
+			if (answer == true) {
+				return i;
+			}
+
+		}
+		return -1;
+
 	}
 
 }
